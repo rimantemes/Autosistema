9df578a83fa1731eaa6775fd0aaa67f6e0e5cd34
Rimante Mes <rimante.mesliute@gmail.com>
2016-08-29 20:36:15 +0300

    Adding my project

---
+++ Auto_sistema/Add_employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Auto_sistema
+{
+/// <summary>
+/// Class adds employees to database
+/// </summary>
+    class Add_employee
+    {
+        private string vardas;
+        private string pavarde;
+
+        public Add_employee(string vardas, string pavarde)
+        {
+            this.vardas = vardas;
+            this.pavarde = pavarde;   
+        }
+
+        public void saveAuto()
+        {
+            var connection = System.Configuration.ConfigurationManager.ConnectionStrings["ServerAttach"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                conn.Open();
+                string cmd = "INSERT INTO [Darbuotojai] (Vardas, Pavarde) " +
+                                        "Values (@Vardas, @Pavarde)";
+                using (SqlCommand at = new SqlCommand(cmd, conn))
+                {
+                    at.Parameters.AddWithValue("@Vardas", vardas);
+                    at.Parameters.AddWithValue("@Pavarde", pavarde);
+
+                    at.ExecuteNonQuery();
+                }
+                // conn.Close();
+            }
+        }
+    }
+}
---
+++ Auto_sistema/expenses.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Auto_sistema
+{
+    /// <summary>
+    /// Class adds expenses to database
+    /// </summary>
+    class expenses
+    {
+     //   private string kuras;
+        private string kitos;
+        private string remontas;
+
+    public expenses(string kitos, string remontas)
+        {
+            //this.kuras = kuras;
+            this.kitos = kitos;
+            this.remontas = remontas;
+        }
+
+    public void add_expenses(int id)
+        {
+            var connection = System.Configuration.ConfigurationManager.ConnectionStrings["ServerAttach"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                conn.Open();
+                string cmd = "UPDATE [Lenta] SET Remonto_islaidos=@Remonto_islaidos, Kitos_islaidos=@Kitos_islaidos WHERE Id=@autoid";
+                
+                using (SqlCommand at = new SqlCommand(cmd, conn))
+                {
+                    at.Parameters.AddWithValue("@Remonto_islaidos", remontas);
+                    at.Parameters.AddWithValue("@Kitos_islaidos", kitos);
+                    at.Parameters.AddWithValue("@autoid", id);
+
+                    at.ExecuteNonQuery();
+                }
+                // conn.Close();
+            }
+        }
+    }
+}
